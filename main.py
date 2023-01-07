ab4a8102dd3f0fe432027295bbc804dac2779cbe
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2023-01-07 15:02:42 +0530

    modified main deploy app

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def hello_world():
-    if flask.render.method == "GET":
-        return (flask.render_template("index.html"))
+    # if flask.render.method == "GET":
+    return (flask.render_template("index.html"))
 
 if __name__ == "__main__":
     app.run()
