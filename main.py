9bb013b9650814536c739aa6edbf2f6409254e74
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2023-01-07 14:46:49 +0530

    added github code

---
+++ main.py
@@ -0,0 +1,11 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=["GET"])
+def hello_world():
+    if flask.render.method == "GET":
+        return (flask.render_template("index.html"))
+
+if __name__ == "__main__":
+    app.run()
